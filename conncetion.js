2799c50aae534b8cab6ddfb15cbf647b6be2b98e
tobizinho <rs2508018@gmail.com>
2024-04-10 17:36:47 -0300

    Update version wweb 2.2322.15

--- conncetion.js
+++ conncetion.js
@@ -2,14 +2,13 @@ const qrcode = require("qrcode-terminal");
 
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
-const wwebVersion = "2.2410.1";
+const wwebVersion = "2.2322.15";
 
 client = new Client({
   authStrategy: new LocalAuth("creds"),
   webVersionCache: {
     type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2322.15.html",
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
   },
   puppeteer: {
     headless: true,
