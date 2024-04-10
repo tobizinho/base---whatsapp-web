6ce17edce2a02940eec20b0cf59317ff8890c708
tobizinho <rs2508018@gmail.com>
2024-04-10 17:33:49 -0300

    first commit

---
+++ conncetion.js
@@ -0,0 +1,28 @@
+const qrcode = require("qrcode-terminal");
+
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+const wwebVersion = "2.2410.1";
+
+client = new Client({
+  authStrategy: new LocalAuth("creds"),
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2322.15.html",
+  },
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  }, // Make headless true or
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.initialize();
